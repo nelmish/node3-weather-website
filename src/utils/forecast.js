3c952c4161f165f65b105c92ac94c754d22e95fe
nelmish <standarduser@fitnessunchained.de>
2021-07-17 19:56:15 +0200

    new forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=9691a75c2815b9d98fa1c64fb02ae705&query=" + latitude + "," + longitude;
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location", undefined);
         } else {
-            callback(undefined, `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is at ${body.current.humidity}%.`);
         }
     })
 };
